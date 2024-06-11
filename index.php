0c2f75e29508f5e1a4ab657e6c53cdb0671f0dd0
cchavezlo <cchavez@incalpaca.com>
2024-06-11 17:46:04 -0500

    Opt2

--- index.php
+++ index.php
@@ -6,6 +6,13 @@
 </head>
 <body>
     <h1>Personajes de Marvel</h1>
- 
+
+    <input type="text" id="characterName" placeholder="Buscar personaje">
+    <button id="searchButton">Buscar</button>
+
+    <div id="results"></div>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="js/script.js"></script>
 </body>
 </html>
